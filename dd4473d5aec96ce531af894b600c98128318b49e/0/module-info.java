2553490860fee9324c5e773bbe786267383f5d04
Fer Madeiral <fer.madeiral@gmail.com>
2021-12-01 01:04:57 +0000

    [Automatic commit] Checkstyle violations from leadpony/joy

---
+++ dd4473d5aec96ce531af894b600c98128318b49e/0/module-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2019-2020 the Joy Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Defines the provider of Jakarta JSON Processing API (JSON-P).
+ */
+module org.leadpony.joy.classic {
+    requires org.leadpony.joy.core;
+
+    provides jakarta.json.spi.JsonProvider
+        with org.leadpony.joy.classic.ClassicJsonProvider;
+}
