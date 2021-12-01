dd566205da53e29f2c9690451575fc86324cd107
Fer Madeiral <fer.madeiral@gmail.com>
2021-12-01 02:16:25 +0000

    [Automatic commit] Checkstyle violations from dangdangdotcom/cymbal

---
+++ e3a293f23667ce209715de20a4e1f023cb51c869/6/ShellCommand.java
@@ -0,0 +1,35 @@
+package com.dangdang.cymbal.service.util.enums;
+
+/**
+ * All support commands for shell service.
+ *
+ * @auther GeZhen
+ */
+public enum ShellCommand {
+
+    APPLY,
+
+    START,
+
+    STOP,
+
+    CREATE_CLUSTER,
+
+    SLAVEOF,
+
+    REDIS_CLI_CMD,
+
+    CONFIG,
+
+    CHECK,
+
+    IMPORT,
+
+    CLUSTER_MEET,
+
+    CLUSTER_REPLICATE,
+
+    MONITOR,
+
+    ;
+}
